60c017ff7279c200ac8776e481d7d1cbb23cd78e
Heba <hebaehab001@gmail.com>
2026-03-31 11:21:29 +0200

    feat: create profile page with user session display

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import PageTitle from "@/components/layout/Common/PageTitle/PageTitle";
+import ProfileSection from "@/components/layout/Profile/ProfileSection";
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <section className="bg-gray-100 min-h-[90vh] py-4 flex flex-col gap-3 items-center w-full">
+      <PageTitle title="My Account" />
+      <ProfileSection />
+    </section>
+  );
 }
---
+++ src/components/layout/Profile/ProfileSection.jsx
@@ -0,0 +1,82 @@
+"use client";
+import React from "react";
+import { useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+export default function ProfileSection() {
+  const { data: session, status } = useSession();
+
+  return (
+    <div className="grid grid-cols-12 w-[90%] gap-3 ">
+      <div className="bg-white rounded-xl shadow-lg  h-fit col-span-12 lg:col-span-4">
+        <div className="relative w-full h-[65vh] flex flex-col items-center bg-white rounded-2xl">
+          <div className="h-[40%] w-full bg-linear-to-b from-sky-800 to-sky-950 rounded-t-2xl">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" width="100%">
+              <rect fill="#ffffff" width={540} height={450} />
+              <defs>
+                <linearGradient
+                  id="a"
+                  gradientUnits="userSpaceOnUse"
+                  x1={0}
+                  x2={0}
+                  y1={0}
+                  y2="100%"
+                  gradientTransform="rotate(222,648,379)"
+                >
+                  <stop offset={0} stopColor="#ffffff" />
+                  <stop offset={1} stopColor="#FC726E" />
+                </linearGradient>
+                <pattern
+                  patternUnits="userSpaceOnUse"
+                  id="b"
+                  width={300}
+                  height={250}
+                  x={0}
+                  y={0}
+                  viewBox="0 0 1080 900"
+                >
+                  <g fillOpacity="0.5">
+                    
+                  </g>
+                </pattern>
+              </defs>
+              <rect x={0} y={0} fill="url(#a)" width="100%" height="100%" />
+              <rect x={0} y={0} fill="url(#b)" width="100%" height="100%" />
+            </svg> */}
+          </div>
+
+          <div className="absolute w-28 h-28 bg-white rounded-full flex justify-center items-center top-[40%] transform -translate-y-1/2">
+            <div className="bg-linear-to-b from-sky-800 to-sky-950 px-10 py-8 rounded-full text-white text-4xl">
+              {session?.user?.name?.charAt(0).toUpperCase()}
+            </div>
+          </div>
+          <div className="px-6 md:px-8 mt-18 text-center flex flex-col items-center w-full">
+            <div className=" font-bold text-2xl text-black ">
+              {session?.user?.name}
+            </div>
+            <div className="mt-2 font-normal text-sm text-gray-600">
+              {session?.user?.email}
+            </div>
+
+            <div className="mt-4 flex flex-col w-full gap-2">
+              <Button
+                variant="outline"
+                className="w-full  text-sky-900 cursor-pointer md:text-lg md:py-5 rounded-2xl hover:bg-transparent hover:text-sky-900"
+              >
+                Edit Profile
+              </Button>
+              <Button
+                variant="outline"
+                className="bg-linear-to-b from-sky-800 to-sky-950 text-white rounded-2xl   w-full border-0 cursor-pointer hover:text-white  md:text-lg md:py-5 "
+              >
+                Security Settings
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white rounded-xl flex flex-col gap-4 shadow-lg p-6 md:p-8 col-span-12 lg:col-span-8">
+        rg
+      </div>
+    </div>
+  );
+}
