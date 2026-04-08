a3131297dad5814d58ad8b1981c0a88b6aa6ed23
Heba <hebaehab001@gmail.com>
2026-04-08 14:07:37 +0200

    feat: update logo component

--- src/components/layout/Common/Logo/Logo.jsx
+++ src/components/layout/Common/Logo/Logo.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Link from "next/link";
-import { RiShoppingBag4Fill } from "react-icons/ri";
+import Image from "next/image";
 export default function Logo() {
   return (
     <Link
       href="/"
       className="flex items-center space-x-2 text-white text-2xl md:text-3xl"
     >
-      <RiShoppingBag4Fill className="m-0" />
+      <Image src='/logo.png' width={25} height={25} alt="logo"/>
       <span className="self-center  font-semibold whitespace-nowrap ">
-        E-commerce
+        Fresh Cart
       </span>
     </Link>
   );
